5cb3f8ce7053b8d091e149136be5b6fa5c7a47a8
Hossein <mhusseinkarimi@gmail.com>
2022-09-02 17:59:38 +0430

    Updates src.utils.sweep to include shfting window option

--- src/utils/sweep.py
+++ src/utils/sweep.py
@@ -1,7 +1,7 @@
 from typing import List
 import numpy as np
 
-def sweepStep(srcShape, steps, endpoint=True) -> List:
+def sweepStep(srcShape, steps, offset=0, endpoint=True) -> List:
     """Sweeping windows over and image to perform localized operations.
 
     Parameters
@@ -11,6 +11,9 @@ def sweepStep(srcShape, steps, endpoint=True) -> List:
     steps : List of tuple or array of integers
         steps of sweeping window in each dimension. Number of the elements should
         be lesser or equal to array dimension
+    offset : int 
+        The offset applied to the start of signal for windows. Negative offsets mean shifting the window 
+        to the left and possitive offsets mean shifting window to right.
     endpoint : bool, optional
         Choice of including or excluding end points at each axis, by default True
 
@@ -32,13 +35,17 @@ def sweepStep(srcShape, steps, endpoint=True) -> List:
     
     sweepSteps = []
     for i in range(ndim):
-        sweepSteps.append(np.arange(0, srcShape[i], steps[i]))
+        tempSteps = np.arange(offset, srcShape[i], steps[i])
+        if offset < 0:
+            tempSteps = tempSteps[tempSteps > 0] 
+            tempSteps = np.insert(tempSteps, 0, 0)
+        sweepSteps.append(tempSteps)
         if endpoint and (srcShape[i] - 1) != sweepSteps[i][-1]:
             sweepSteps[i] = np.append(sweepSteps[i], srcShape[i]-1)
     return sweepSteps
 
 if __name__ == "__main__":
-    # Test case: [array([   0,  200,  400,  600,  800, 1000, 1200, 1400, 1600, 1622]), array([   0,  300,  600,  900, 1200, 1454])]
-    print(sweepStep((1623, 1455, 3), [200, 300]))
-    # Test case: [array([   0,  200,  400,  600,  800, 1000, 1200, 1400, 1600]), array([   0,  300,  600,  900, 1200])]
-    print(sweepStep((1623, 1455, 3), [200, 300], endpoint=False))
+    # Test case: [array([  30,  230,  430,  630,  830, 1030, 1230, 1430, 1622]), array([  30,  330,  630,  930, 1230, 1454])]
+    print(sweepStep((1623, 1455, 3), [200, 300], offset=30, endpoint=True))
+    # Test case: [array([   0,  196,  396,  596,  796,  996, 1196, 1396, 1596]), array([   0,  296,  596,  896, 1196])]
+    print(sweepStep((1623, 1455, 3), [200, 300], offset=-4, endpoint=False))
