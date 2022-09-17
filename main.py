35e829365ad334af1345886b27ad6dd01a3a621b
Hossein <mhusseinkarimi@gmail.com>
2022-09-17 19:07:24 +0430

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ img_path = "/home/curiouscoder/Downloads/spot detection project material/test1_7
 img = cv.imread(img_path)
 
 detector = WatershedDetector(sweep=True, threshStep=(400, 400), watershedStep=(50, 50))
-cents, stats = detector.detect(img, verbose=1, save_path="/home/curiouscoder/Desktop/test1_res.jpg")
+stats = detector.detect(img, verbose=1, save_path="/home/curiouscoder/Desktop/test1_res.jpg")
 detector.stats(
-    img_size_cm=(6.6, 2.1), img_size_pxl=img.shape[:2],
+    img_size_cm=(6.6, 2.6), img_size_pxl=img.shape[:2],
     stats=stats, show_results=True
 )
